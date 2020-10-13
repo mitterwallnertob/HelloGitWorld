b0dc3b1885c759e8a6b3953df900fb0583c32dd7
Tobias Mitterwallner <tobias.mitterwallner@htl-wels.at>
2020-10-13 21:33:18 +0200

    "initial"

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+/*******************************
+ * @class: Main
+ * @author: Tobias Mitterwallner
+ * @date: 12.10.2020
+ ******************************/
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int x = 0;
+        int y = 0;
+        int s;
+        boolean z = false;
+
+        System.out.println("Addition von 2 eingelesenen Werten");
+        System.out.printf("----------------------------------\n");
+
+
+        while(z == false)
+        {
+            try
+            {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Ersten Wert eingeben:");
+                x = sc.nextInt();
+                z = true;
+            }
+
+            catch(Exception e)
+            {
+                System.out.println("Fehler! Bitte erneut versuchen.");
+            }
+        }
+
+
+        while(z == true)
+        {
+            try
+            {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Zweiten Wert eingeben:");
+                y = sc.nextInt();
+                z = false;
+            }
+
+            catch(Exception e)
+            {
+                System.out.println("Fehler! Versuchen Sie es noch einmal!");
+            }
+        }
+
+        s = x + y;
+        System.out.printf("Ergebnis: %d", s);
+    }
+}
